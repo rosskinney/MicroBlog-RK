3f0db6678492761fa5258bc6a9e83872ba386a35
deb <peterson.deb@comcast.net>
2012-10-03 08:38:15 -0600

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroBlogRk::Application.config.secret_token = 'fa7ca7eec18e80dd42ece3c9c4db0088aef42d9c230b9dd6dd9fea5abdd25d3e4092b2fcf62bff8074d22cd31f60295a5c9637d48cf511a14304594fa6f68667'
